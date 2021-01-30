b3ce6ce1b6fa42e9f0880007e8768843471db51d
Wina Wijannah <wina18ti@mahasiswa.pcr.ac.id>
2021-01-30 19:40:20 +0700

    Ini Progres Wina

---
+++ app/src/main/java/com/example/kampus/user.kt
@@ -0,0 +1,11 @@
+package com.example.kampus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class userActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
